7c570178a70b67363ee9a49df30b25762beb16b5
github username <github email address>
2018-12-23 22:22:00 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,31 +2,32 @@ katz_deli = []
 
 
 def line(katz_deli)
-  if katz_deli[0] == nil
-    puts "The line is currently empty."
-  else
-    linearr = []
-    counter = 1 
-    katz_deli.each do |name|
-      linearr.push("#{counter}. #{name}")
-      counter += 1 
-    end
+    if katz_deli[0] == nil
+        puts "The line is currently empty."
+    else
+        linearr = []
+        counter = 1
+        katz_deli.each do |name|
+            linearr << "#{counter}. #{name}"
+            counter += 1
+        end
     puts "The line is currently: #{linearr.join(" ")}"
     return counter, linearr[counter]
-  end
+    end
 end
 
 
+
 def take_a_number(katz_deli, newname)
-  katz_deli.push(newname)
-  puts "Welcome, #{newname}. You are number #{katz_deli.length} in line."
+    katz_deli << newname
+    puts "Welcome, #{newname}. You are number #{katz_deli.length} in line."
 end
 
 
 def now_serving(katz_deli)
-  if katz_deli[0] == nil
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.shift}."
-  end
+    if katz_deli[0] == nil
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli.shift}."
+    end
 end
